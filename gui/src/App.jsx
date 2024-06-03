93020e3e8e6142939dfe97981533a2d980c8d48f
Andrew920528 <andrewhsu.0528@gmail.com>
2024-06-03 14:32:44 +0800

    font and typography

--- gui/src/App.jsx
+++ gui/src/App.jsx
@@ -9,7 +9,19 @@ function App() {
     }
     runRequest();
   }, []);
-  return <div className="App">Good Night Ojosama</div>;
+  return (
+    <div className="App">
+      Good Night Ojosama
+      <h4>
+        <strong>筆記This is h4 with strong, font size = 20</strong>
+      </h4>
+      <p>This is a p, font size = 16</p>
+      <div>This is a div, font size = 14</div>
+      <div className="--label">Some text</div>
+      <div className="--heading">新科目 Hello</div>
+      <div>新科目 Hello</div>
+    </div>
+  );
 }
 
 export default App;
