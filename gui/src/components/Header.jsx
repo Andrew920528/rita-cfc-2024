20d4053a88946a61e669cc995031d7eab3c3fbb0
Andrew920528 <andrewhsu.0528@gmail.com>
2024-06-10 14:54:29 +0800

    menu implemented

--- gui/src/components/Header.jsx
+++ gui/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import IconButton from "./ui_components/IconButton";
-import FloatingMenu from "./ui_components/FloatingMenu";
+import {FloatingMenuButton} from "./ui_components/FloatingMenu";
 import {
   Close,
   User,
@@ -9,79 +9,12 @@ import {
   Settings,
   Logout,
   Menu,
+  UserAvatar,
 } from "@carbon/icons-react";
 import Dropdown from "./ui_components/Dropdown";
 
-const dummy = {
-  0: {
-    id: "option-0",
-    text: "Lorem, ipsum dolorawjenfla neawnflajwenfawjenfajwenfkawnefkajnw sit amet consectetur adipisicing elit.",
-  },
-  1: {
-    id: "option-1",
-    text: "Option 1",
-  },
-  2: {
-    id: "option-2",
-    text: "Option 2",
-  },
-  3: {
-    id: "option-3",
-    text: "Option 3 - a disabled item",
-    disabled: true,
-  },
-  4: {
-    id: "option-4",
-    text: "Option 4",
-  },
-  5: {
-    id: "option-5",
-    text: "Option 5",
-  },
-};
-
-const AccountMenu = () => {
-  const AccountContent = ({
-    name = "廖偉良",
-    occupation = "級任老師",
-    school = "松山高中",
-  }) => {
-    return (
-      <div className="account-content">
-        <div className="user-info">
-          <p> {name} </p>
-          <p>
-            {" "}
-            {occupation} @ {school}
-          </p>
-        </div>
-        <IconButton
-          flex={true}
-          text={"管理帳號"}
-          icon={<Settings />}
-          mode={"ghost"}
-          onClick={() => {
-            console.log("Manage Account");
-          }}
-        />
-        <IconButton
-          flex={true}
-          text={"登出"}
-          icon={<Logout />}
-          mode={"ghost"}
-          onClick={() => {
-            console.log("Log out");
-          }}
-        />
-      </div>
-    );
-  };
-  return <FloatingMenu content={<AccountContent />} />;
-};
-
 const Header = ({openNav, setOpenNav = () => {}}) => {
   const [session, setSession] = useState(-1);
-  const [openAccountMenu, setOpenAccountMenu] = useState(false);
   return (
     <div className="header">
       <div className="header-left">
@@ -123,18 +56,92 @@ const Header = ({openNav, setOpenNav = () => {}}) => {
             }
           />
         </div>
-        <div className="account-button">
-          <IconButton
-            mode={"on-dark"}
-            icon={<User size={20} />}
-            onClick={() => {
-              setOpenAccountMenu(!openAccountMenu);
-            }}
-          />
-          {openAccountMenu && <AccountMenu />}
-        </div>
+        <AccountButton />
       </div>
     </div>
+  );
+};
+
+const dummy = {
+  0: {
+    id: "option-0",
+    text: "Lorem, ipsum dolorawjenfla neawnflajwenfawjenfajwenfkawnefkajnw sit amet consectetur adipisicing elit.",
+  },
+  1: {
+    id: "option-1",
+    text: "Option 1",
+  },
+  2: {
+    id: "option-2",
+    text: "Option 2",
+  },
+  3: {
+    id: "option-3",
+    text: "Option 3 - a disabled item",
+    disabled: true,
+  },
+  4: {
+    id: "option-4",
+    text: "Option 4",
+  },
+  5: {
+    id: "option-5",
+    text: "Option 5",
+  },
+};
+
+const AccountButton = () => {
+  const AccountContent = ({
+    name = "廖偉良",
+    occupation = "級任老師",
+    school = "松山高中",
+  }) => {
+    return (
+      <div className="account-content">
+        <div className="user-info">
+          <div className="user-info-col">
+            <p>
+              <strong>{name}</strong>
+            </p>
+            <p className="user-detail">
+              {occupation} @ {school}
+            </p>
+          </div>
+          <UserAvatar size={30} />
+        </div>
+        <IconButton
+          flex={true}
+          text={"管理帳號"}
+          icon={<Settings />}
+          mode={"on-dark-2"}
+          onClick={() => {
+            console.log("Manage Account");
+          }}
+        />
+        <IconButton
+          flex={true}
+          text={"登出"}
+          icon={<Logout />}
+          mode={"on-dark-2"}
+          onClick={() => {
+            console.log("Log out");
+          }}
+        />
+      </div>
+    );
+  };
+
+  const menuProps = {
+    mode: "dark",
+    content: <AccountContent />,
+  };
+  return (
+    <FloatingMenuButton
+      button={<IconButton mode="on-dark" icon={<User size={20} />} />}
+      menuProps={menuProps}
+      anchorOrigin={{horizontal: "right", vertical: "bottom"}}
+      transformOrigin={{horizontal: "right", vertical: "top"}}
+    />
   );
 };
 
--- gui/src/components/ui_components/FloatingMenu.jsx
+++ gui/src/components/ui_components/FloatingMenu.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, {useRef, useState, useEffect} from "react";
 
-const FloatingMenu = ({content}) => {
-  return <div className="floating-menu">{content}</div>;
+export const FloatingMenu = ({content, mode}) => {
+  return <div className={`floating-menu ${mode}`}>{content}</div>;
 };
 
-export default FloatingMenu;
+export const FloatingMenuButton = ({
+  button,
+  menuProps,
+  anchorOrigin = {
+    vertical: "top",
+    horizontal: "left",
+  },
+  transformOrigin = {
+    vertical: "bottom",
+    horizontal: "right",
+  },
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`fm-button a-${anchorOrigin.horizontal} a-${anchorOrigin.vertical} t-${transformOrigin.horizontal} t-${transformOrigin.vertical}`}
+      ref={menuRef}
+    >
+      <div
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        {button}
+      </div>
+
+      {isOpen && (
+        <div className="fm-wrapper">
+          <FloatingMenu {...menuProps} />
+        </div>
+      )}
+    </div>
+  );
+};
