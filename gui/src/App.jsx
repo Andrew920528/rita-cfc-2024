e5bd8ba098ac34020f82ebd69d64a94c1e8cca71
ellenshaw <ellen921002@gmail.com>
2024-06-12 07:24:03 -0600

    Buggy code

--- gui/src/App.jsx
+++ gui/src/App.jsx
@@ -2,16 +2,18 @@ import {useEffect} from "react";
 import "./style/main.scss";
 import {tryTrySee} from "./utils/service";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 function App() {
   // useEffect(() => {
   //   async function runRequest() {
   //     await tryTrySee();
   //   }
   //   runRequest();
-  // }, []);
+  // }, []); <Login></Login> <Home></Home> 
   return (
     <div className="App">
-      <Home></Home>
+      <Login></Login> 
+      
     </div>
   );
 }
