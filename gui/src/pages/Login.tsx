c80609e60ab05fcb4b11d877498e2d69fae0419e
Andrew920528 <andrewhsu.0528@gmail.com>
2024-06-13 22:59:51 +0800

    added notes for ellen

--- gui/src/pages/Login.tsx
+++ gui/src/pages/Login.tsx
@@ -1,5 +1,13 @@
 import React, {useEffect, useState} from "react";
 
+/**
+ * Notes for Ellen:
+ * 1. To keep style consistent, use <Textbox> component
+ * 2. Before we proceed with the data flow, let's look at the ui components
+ * 3. Let's add a button with <InputButton>, which is a wrapper around <button>
+ * 4. Now, let's change the css to make it look better
+ * 5. Finally, we will look at how to store input as states
+ */
 const Login = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
