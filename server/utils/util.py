485c67965520c8aefdbf84b118aadc1edad28baf
Andrew920528 <andrewhsu.0528@gmail.com>
2024-07-17 16:08:52 +0800

    [wip] attempt to refactor & make scalable pipelline

--- server/utils/util.py
+++ server/utils/util.py
@@ -12,19 +12,6 @@ def logTime(start_time, description):
     logger.info(f"Time = {now_formatted}, time passed = {latency_formatted} | {description}")
     
     
-def extract_json_as_dict(full_string):
-    # Find the starting and ending points of the JSON portion
-    start_index = full_string.find('{')
-    end_index = full_string.rfind('}') + 1
-
-    # Extract the JSON string
-    json_string = full_string[start_index:end_index]
-
-    # Convert the JSON string to a dictionary
-    json_dict = json.loads(json_string)
-
-    return json_dict
-
 def split_chunk(text, max_length):
     """Splits a string by space, (most) chinese characters, and break up strings longer than max_length
 
