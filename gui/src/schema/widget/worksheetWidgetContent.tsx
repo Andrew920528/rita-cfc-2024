a513af2ebfe012760adae0802907614caedecaac
Andrew920528 <andrewhsu.0528@gmail.com>
2024-07-23 15:08:31 +0800

    added plain worksheet widget

---
+++ gui/src/schema/widget/worksheetWidgetContent.tsx
@@ -0,0 +1,25 @@
+import {Alarm, Document} from "@carbon/icons-react";
+import {WidgetType} from "./widget";
+import {WidgetMaker} from "./widget";
+
+export type WorksheetWidgetContent = {}; // currently content comes from user
+
+export class WorksheetWidgetMaker extends WidgetMaker<WorksheetWidgetContent> {
+  init() {
+    return {};
+  }
+  isType(obj: any): obj is WorksheetWidgetContent {
+    return true;
+  }
+  uiBook() {
+    return {
+      title: "學習單",
+      hint: "製作學習單",
+      icon: <Document />,
+      type: WidgetType.Worksheet,
+      width: 300,
+      minHeight: 300,
+      maxHeight: 500,
+    };
+  }
+}
