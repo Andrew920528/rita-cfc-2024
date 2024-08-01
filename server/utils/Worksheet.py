7eeec4a536fc7a69aaaddc8b56e5d70a950ff159
Alan Hou <alanhou911222@gmail.com>
2024-08-01 17:23:03 +0800

    add 'addMatching' worksheet function

--- server/utils/Worksheet.py
+++ server/utils/Worksheet.py
@@ -2,6 +2,9 @@ from pylatex import Document, Command, Package
 from pylatex.base_classes import Environment
 from pylatex.utils import NoEscape
 
+class MatchTabularEnvironment(Environment):
+    _latex_name = "matchtabular"
+
 class Worksheet:
     def __init__(self, title, author, date):
         self.doc = Document(documentclass='ctexart')
@@ -15,6 +18,24 @@ class Worksheet:
 
         self.doc.append(NoEscape(r"\maketitle"))
 
-    def print_doc(self):
-        self.doc.generate_pdf(self.title, compiler='xelatex', clean=True,  clean_tex=False)
+    def addMatching(self, pairs):
+        self.doc.append(NoEscape(r'\newcounter{matchleft}'))
+        self.doc.append(NoEscape(r'\newcounter{matchright}'))
+        self.doc.append(NoEscape(r'''\newenvironment{matchtabular}{%
+            \setcounter{matchleft}{0}%
+            \setcounter{matchright}{0}%
+            \tabularx{\textwidth}{%
+                >{\leavevmode\hbox to 1.5em{\stepcounter{matchleft}\arabic{matchleft}.}}X%
+                >{\leavevmode\hbox to 1.5em{\stepcounter{matchright}\alph{matchright})}}X%
+            }%
+        }{\endtabularx}'''))
+                
+        str = ""
+        for left, right in pairs:
+            str += left + " & " + right + r" \\"
         
+        with self.doc.create(MatchTabularEnvironment()) as environment:
+            environment.append(NoEscape(str))
+
+    def printDoc(self):
+        self.doc.generate_pdf(self.title, compiler='xelatex', clean=True,  clean_tex=False)
