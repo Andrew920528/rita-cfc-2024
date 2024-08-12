1d8ee095066785f908e472cebee81bef36889a9a
Andrew920528 <andrewhsu.0528@gmail.com>
2024-08-12 11:06:59 +0800

    word documents and pdf can be generated

--- server/utils/Worksheet.py
+++ server/utils/Worksheet.py
@@ -4,14 +4,19 @@ from pylatex.utils import NoEscape
 from pdf2docx import Converter
 import os.path
 
+
 class MatchTabularEnvironment(Environment):
     _latex_name = "matchtabular"
 
+
 class Worksheet:
-    def __init__(self, title, author, date):
-        geometry_options = {"lmargin" : "1in", "rmargin" : "1in", "tmargin" : "1in", "bmargin" : "1in", "headheight" : "14pt"}
-        self.doc = Document(documentclass='ctexart', geometry_options=geometry_options)
+    def __init__(self, path, title, author, date):
+        geometry_options = {"lmargin": "1in", "rmargin": "1in",
+                            "tmargin": "1in", "bmargin": "1in", "headheight": "14pt"}
+        self.doc = Document(documentclass='ctexart',
+                            geometry_options=geometry_options)
         self.title = title
+        self.path = path
 
         self.doc.preamble.append(Package('tabularx'))
         self.doc.preamble.append(Package('titling'))
@@ -28,7 +33,7 @@ class Worksheet:
 
     def addSubsection(self, title):
         self.doc.append(Subsection(title))
-    
+
     def addText(self, text):
         self.doc.append(text)
         self.doc.append(LineBreak())
@@ -45,7 +50,7 @@ class Worksheet:
         self.doc.append(NoEscape(r'\newcounter{matchleft}'))
         self.doc.append(NoEscape(r'\newcounter{matchright}'))
         self.doc.append(NoEscape(
-        r"""\newenvironment{matchtabular}{%
+            r"""\newenvironment{matchtabular}{%
             \setcounter{matchleft}{0}%
             \setcounter{matchright}{0}%
             \tabularx{\textwidth}{%
@@ -53,22 +58,23 @@ class Worksheet:
                 >{\leavevmode\hbox to 1.5em{\stepcounter{matchright}\alph{matchright})}}X%
             }%
         }{\endtabularx}"""))
-                
+
         str = ""
         for left, right in pairs:
             str += left + " & " + right + r" \\"
-        
+
         with self.doc.create(MatchTabularEnvironment()) as environment:
             environment.append(NoEscape(str))
         self.doc.append(LineBreak())
-    
+
     def generatePDF(self):
-        self.doc.generate_pdf(self.title, compiler='xelatex', clean=True, clean_tex=False)
-    
+        self.doc.generate_pdf(self.path, compiler='xelatex',
+                              clean=True, clean_tex=False)
+
     def generateDoc(self):
-        pdfPath = "./" + self.title + ".pdf"
-        docxPath = "./" + self.title + ".docx"
-        
+        pdfPath = self.path + ".pdf"
+        docxPath = self.path + ".docx"
+
         self.generatePDF()
         cv = Converter(pdfPath)
         cv.convert(docxPath, start=0, end=None)
