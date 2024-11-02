11627a4f2dcb9e23cf961982e6376018cf659d62
Andrew920528 <andrewhsu.0528@gmail.com>
2024-11-02 18:44:57 +0800

    added config files to frontend

--- gui/src/global/constants.ts
+++ gui/src/global/constants.ts
@@ -1,6 +1,9 @@
 export const INDEPENDENT_MODE: boolean =
   import.meta.env.VITE_GUI_INDEPENDENT === "true" ? true : false;
 
+export const BASE_URL_DEV: string =
+  import.meta.env.VITE_BACKEND_HOST || "http://localhost:3000";
+
 export enum API {
   ERROR = "error",
   SUCCESS = "success",
